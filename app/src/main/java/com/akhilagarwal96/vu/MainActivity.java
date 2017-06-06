0961af800b039a1132fbc2cc9e520c585cfcc1fe
Akhil Agarwal <akhilagarwal96@gmail.com>
2017-06-06 23:20:21 +0530

    Profile

--- app/src/main/java/com/akhilagarwal96/vu/MainActivity.java
+++ app/src/main/java/com/akhilagarwal96/vu/MainActivity.java
@@ -1,7 +1,6 @@
 package com.akhilagarwal96.vu;
 
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.FrameLayout;
@@ -16,11 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+       /* toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Nav_Draw nav_draw = (Nav_Draw) getSupportFragmentManager().findFragmentById(R.id.nav_draw);
         nav_draw.setUp(R.id.nav_draw, (DrawerLayout) findViewById(R.id.draw_layout), toolbar);
-        frameLayout = (FrameLayout) findViewById(R.id.frame);
+        frameLayout = (FrameLayout) findViewById(R.id.frame);*/
     }
 }
