57975278a3b56aab5cae1cbad5d3e88881008bbe
Akhil Agarwal <akhilagarwal96@gmail.com>
2017-06-15 17:22:29 +0530

    home

--- app/src/main/java/com/akhilagarwal96/vu/CgpaCal.java
+++ app/src/main/java/com/akhilagarwal96/vu/CgpaCal.java
@@ -1,14 +1,9 @@
 package com.akhilagarwal96.vu;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.LoginFilter;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
