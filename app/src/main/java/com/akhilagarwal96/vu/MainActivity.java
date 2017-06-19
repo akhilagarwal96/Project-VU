e6f7dc2d3d37136aa7536e9778a3db3da5633de8
Akhil Agarwal <akhilagarwal96@gmail.com>
2017-06-19 11:46:28 +0530

    Bug fixed

--- app/src/main/java/com/akhilagarwal96/vu/MainActivity.java
+++ app/src/main/java/com/akhilagarwal96/vu/MainActivity.java
@@ -13,8 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
-
+        setContentView(R.layout.activity_main);
 /*
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
