04c2f0754c9a26fd64491bb84e9de6638f03d252
Akhil Agarwal <akhilagarwal96@gmail.com>
2017-06-19 11:44:18 +0530

    Home issue solved

--- app/src/main/java/com/akhilagarwal96/vu/MainActivity.java
+++ app/src/main/java/com/akhilagarwal96/vu/MainActivity.java
@@ -13,13 +13,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.test);
 
-       /* toolbar = (Toolbar) findViewById(R.id.toolbar);
+/*
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Nav_Draw nav_draw = (Nav_Draw) getSupportFragmentManager().findFragmentById(R.id.nav_draw);
         nav_draw.setUp(R.id.nav_draw, (DrawerLayout) findViewById(R.id.draw_layout), toolbar);
-        frameLayout = (FrameLayout) findViewById(R.id.frame);*/
+        frameLayout = (FrameLayout) findViewById(R.id.frame);
+*/
     }
 }
