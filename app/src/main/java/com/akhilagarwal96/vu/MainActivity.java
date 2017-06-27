03c03e8f9d6930f46a87fa1e5996829860062595
Akhil Agarwal <akhilagarwal96@gmail.com>
2017-06-27 14:22:09 +0530

    Login Integrated

--- app/src/main/java/com/akhilagarwal96/vu/MainActivity.java
+++ app/src/main/java/com/akhilagarwal96/vu/MainActivity.java
@@ -1,6 +1,7 @@
 package com.akhilagarwal96.vu;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.FrameLayout;
@@ -13,14 +14,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
-/*
+        setContentView(R.layout.activity_main);
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         Nav_Draw nav_draw = (Nav_Draw) getSupportFragmentManager().findFragmentById(R.id.nav_draw);
         nav_draw.setUp(R.id.nav_draw, (DrawerLayout) findViewById(R.id.draw_layout), toolbar);
         frameLayout = (FrameLayout) findViewById(R.id.frame);
-*/
     }
 }
